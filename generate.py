714017ff11fd3674a4f015cb9aece5d970e6f499
wolframalexa <alexajakob@tutanota.com>
2022-02-20 12:10:13 -0500

    voronoi with points

--- generate.py
+++ generate.py
@@ -2,6 +2,15 @@
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
+
+def points_in_circle_np(radius, x0=0, y0=0, ):
+	# finds integer points on a given circle
+	x_ = np.arange(x0 - radius - 1, x0 + radius + 1, dtype=int)
+	y_ = np.arange(y0 - radius - 1, y0 + radius + 1, dtype=int)
+	x, y = np.where((x_[:,np.newaxis] - x0)**2 + (y_ - y0)**2 <= radius**2)
+	for x, y in zip(x_[x], y_[y]):
+		yield x, y
+
 numcircles = 1
 numdots = 3
 x = 30 # x dimension of inflatable [cm]
@@ -15,6 +24,8 @@ dot1 = [5, 2]
 dot2 = [20, 18]
 dot3 = [15, 26]
 
+circ_pts = list(points_in_circle_np(rad, x0=circ[0], y0 = circ[1]))
+
 # plot inflatable
 plt.axis([0, 30, 0, 30])
 plt.gca().set_aspect('equal', adjustable='box')
@@ -26,8 +37,12 @@ plt.plot(dot1[0], dot1[1], 'ro')
 plt.plot(dot2[0], dot2[1], 'ro')
 plt.plot(dot3[0], dot3[1], 'ro')
 
+plt.plot(circ_pts)
+
+
 # plots voronoi diagram
-points = np.array([dot1, dot2, dot3, circ]) # TODO: incorporate circle into diagram
+points = np.array([dot1, dot2, dot3]) # TODO: incorporate circle into diagram
+points = np.concatenate((points, np.array(circ_pts))
 vor = Voronoi(points)
 fig = voronoi_plot_2d(vor)
 plt.show()
