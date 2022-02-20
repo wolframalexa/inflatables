72698338cb46bbf2acd62dd78533044e72393eb4
wolframalexa <alexajakob@tutanota.com>
2022-02-20 11:29:56 -0500

    initial commit

---
+++ generate.py
@@ -0,0 +1,33 @@
+# generates positions for circles and dots for the inflatable given some input parameters
+import matplotlib.pyplot as plt
+
+numcircles = 1
+numdots = 3
+x = 30 # x dimension of inflatable [cm]
+y = 30 # y dimension of inflatable [cm]
+
+
+# parameters that should be machine-generated later, but for now, will be set
+rad = 3 # circle radius
+circ = (10, 10) # circle coordinates
+dot1 = (5, 2)
+dot2 = (20, 18)
+dot3 = (15, 26)
+
+# plot inflatable
+plt.axis([0, 30, 0, 30])
+plt.axis("equal")
+
+c = plt.Circle(circ, radius=rad)
+plt.gca().add_artist(c)
+
+plt.plot(dot1[0], dot1[1], 'ro')
+
+
+# function for rejection sampling
+
+
+# perform rejection sampling
+
+
+# 
