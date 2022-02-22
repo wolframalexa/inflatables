57dd1fe0b8e0a720a5b66925066740abda6b700f
wolframalexa <alexajakob@tutanota.com>
2022-02-22 13:38:29 -0500

    model circle as hexagon

--- generate.py
+++ generate.py
@@ -1,15 +1,19 @@
 # generates positions for circles and dots for the inflatable given some input parameters
 import matplotlib.pyplot as plt
 from scipy.spatial import Voronoi, voronoi_plot_2d
+import numpy as np
+import math
 
-
-def points_in_circle_np(radius, x0=0, y0=0, ):
-	# finds integer points on a given circle
-	x_ = np.arange(x0 - radius - 1, x0 + radius + 1, dtype=int)
-	y_ = np.arange(y0 - radius - 1, y0 + radius + 1, dtype=int)
-	x, y = np.where((x_[:,np.newaxis] - x0)**2 + (y_ - y0)**2 <= radius**2)
-	for x, y in zip(x_[x], y_[y]):
-		yield x, y
+def circle_to_hexagon(r, x, y):
+	a = 0.5*r
+	b = math.sqrt(3)/2 * r
+	pts = [[x - r, y],
+		[x - a, y + b],
+		[x + a, y + b],
+		[x + r, y],
+		[x + a, y - b],
+		[x - a, y - b]]
+	return(pts)
 
 numcircles = 1
 numdots = 3
@@ -24,33 +28,27 @@ dot1 = [5, 2]
 dot2 = [20, 18]
 dot3 = [15, 26]
 
-circ_pts = list(points_in_circle_np(rad, x0=circ[0], y0 = circ[1]))
+circ_pts = circle_to_hexagon(rad, circ[0], circ[1])
+print(circ_pts)
 
-# plot inflatable
-plt.axis([0, 30, 0, 30])
-plt.gca().set_aspect('equal', adjustable='box')
+# # plot inflatable
+# plt.axis([0, 30, 0, 30])
 
-c = plt.Circle(circ, radius = rad)
-plt.gca().add_artist(c)
+# c = plt.Circle(circ, radius = rad)
+# plt.gca().add_artist(c)
 
-plt.plot(dot1[0], dot1[1], 'ro')
-plt.plot(dot2[0], dot2[1], 'ro')
-plt.plot(dot3[0], dot3[1], 'ro')
+# plt.plot(dot1[0], dot1[1], 'ro')
+# plt.plot(dot2[0], dot2[1], 'ro')
+# plt.plot(dot3[0], dot3[1], 'ro')
 
-plt.plot(circ_pts)
+# plt.plot(circ_pts)
 
 
 # plots voronoi diagram
 points = np.array([dot1, dot2, dot3]) # TODO: incorporate circle into diagram
-points = np.concatenate((points, np.array(circ_pts))
+points = np.concatenate((points, np.array(circ_pts)))
 vor = Voronoi(points)
 fig = voronoi_plot_2d(vor)
+plt.axis([0, 30, 0, 30])
+plt.gca().set_aspect('equal', adjustable='box')
 plt.show()
-
-# function for rejection sampling
-
-
-# perform rejection sampling
-
-
-# 
