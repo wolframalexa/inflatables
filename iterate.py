ff21ffa8bc681f6193c964124d7cf78fd55f65e7
wolframalexa <alexajakob@tutanota.com>
2022-02-25 22:11:22 -0500

    wrote iterative logic

--- iterate.py
+++ iterate.py
@@ -1,6 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import Voronoi, voronoi_plot_2d
+import math
+from scipy.stats import multivariate_normal
+from scipy.stats import uniform
 
-# Generates inflatables by iteration through some rules
 
 def followsRules(circles, points):
 	out = True
@@ -69,3 +73,69 @@ def incircle(point, circle):
 		return True
 	else:
 		return False
+
+# Mixture distribution (aka u can make the means and covariance matrix anything that you want)
+def getpx(pos):
+    return multivariate_normal.pdf(pos, [5, 5], [[1, 0], [0, 1]]) + multivariate_normal.pdf(pos, [8, 8], [[1, 0], [0, 1]]) + multivariate_normal.pdf(pos, [2, 9], [[1, 0], [0, 1]])
+
+# Large gaussian encompassing the entire mixture
+def getqx(pos):
+    return multivariate_normal([6, 6], [[12, 0], [0, 12]]).pdf(pos)
+
+
+iterations = 10
+numcircles = 1
+numdots = 2
+
+circles = np.zeros((numcircles, 3))
+dots = np.zeros((numdots, 2))
+
+pro_circles = np.zeros((numcircles, 3)) # proposed circles and dots
+pro_dots = np.zeros((numdots, 2))
+
+# TODO: logic currently doesn't allow for re-use of points - needs to be fixed (eg if a circle is accepted at step 1, it should remain accepted all the way to step 10)
+
+for i in range(0, iterations):
+	# generate circle
+	rad = np.random.uniform(1,3) # random circle radius
+	x = np.random.uniform(1,11)
+	y = np.random.uniform(1,11)
+
+	pro_circles[0] = [x, y, rad]
+
+	# meets configuration? if yes - then accept
+	if followsRules(pro_circles, []):
+		circles = pro_circles
+
+	# generate dots
+	x, y = np.mgrid[0:12:.01, 0:12:.01]
+	pos = np.dstack((x, y))
+
+	k = np.amax(np.divide(getpx(pos), getqx(pos)))
+	samples = np.zeros([numdots + numcircles, 2])
+	i = 0
+
+	while i < numdots:
+	    test = np.zeros([1,2])
+	    test = np.random.multivariate_normal([6, 6], [[12, 0], [0, 12]])
+	    u = np.random.uniform(0, (k*getqx(test)))
+
+	    if u <= getpx(test):
+	        samples[i, :] = test
+	        i+=1
+
+	# check if follows rules
+	if followsRules(pro_circles, samples):
+		points = samples
+
+	# plot the image as it is
+	plt.axis([0, 12, 0, 12])
+
+	for circle in circles:
+		c = plt.Circle((circle[0], circle[1]), radius = circle[2])
+		plt.gca().add_artist(c)
+	plt.scatter(points[:,0], points[:,1])
+	plt.show()
+
+	path = "../plots/" + str(i) + ".jpg"
+	plt.savefig(path)
